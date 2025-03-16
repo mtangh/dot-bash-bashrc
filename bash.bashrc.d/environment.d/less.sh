5105a3ed7eb81f2eea496b00c50b65256e0ac3a4
mtan <mtan@ugoole.org>
2025-03-16 17:51:21 +0900

    less termcap

--- bash.bashrc.d/environment.d/less.sh
+++ bash.bashrc.d/environment.d/less.sh
@@ -17,13 +17,39 @@ export LESSCHARSET
 # Color settings when there is no interactive shell
 if [ -t 0 ]
 then
-  LESS_TERMCAP_mb=$(printf '\e[1;31m');     export LESS_TERMCAP_mb
-  LESS_TERMCAP_md=$(printf '\e[1;34m');     export LESS_TERMCAP_md
-  LESS_TERMCAP_me=$(printf '\e[0m');        export LESS_TERMCAP_me
-  LESS_TERMCAP_so=$(printf '\e[1;33;47m');  export LESS_TERMCAP_so
-  LESS_TERMCAP_se=$(printf '\e[0m');        export LESS_TERMCAP_se
-  LESS_TERMCAP_us=$(printf '\e[4;36m');     export LESS_TERMCAP_us
-  LESS_TERMCAP_ue=$(printf '\e[0m');        export LESS_TERMCAP_ue
+  case "${PAGER:-}" in
+  */less|*/jless)
+    LESS_TERMCAP_mb="$(tput bold; tput setaf 1)"
+    LESS_TERMCAP_md="$(tput bold; tput setaf 4)"
+    LESS_TERMCAP_me="$(tput sgr0)"
+    LESS_TERMCAP_so="$(tput bold; tput setaf 3; tput setab 7)"
+    LESS_TERMCAP_se="$(tput rmso; tput sgr0)"
+    LESS_TERMCAP_us="$(tput smul; tput bold; tput setaf 6)"
+    LESS_TERMCAP_ue="$(tput rmul; tput sgr0)"
+    LESS_TERMCAP_mr="$(tput rev)"
+    LESS_TERMCAP_mh="$(tput dim)"
+    LESS_TERMCAP_ZN="$(tput ssubm)"
+    LESS_TERMCAP_ZV="$(tput rsubm)"
+    LESS_TERMCAP_ZO="$(tput ssupm)"
+    LESS_TERMCAP_ZW="$(tput rsupm)"
+    # Export
+    export LESS_TERMCAP_mb
+    export LESS_TERMCAP_md
+    export LESS_TERMCAP_me
+    export LESS_TERMCAP_so
+    export LESS_TERMCAP_se
+    export LESS_TERMCAP_us
+    export LESS_TERMCAP_ue
+    export LESS_TERMCAP_mr
+    export LESS_TERMCAP_mh
+    export LESS_TERMCAP_ZN
+    export LESS_TERMCAP_ZV
+    export LESS_TERMCAP_ZO
+    export LESS_TERMCAP_ZW
+    ;;
+  *)
+    ;;
+  esac
 fi
 
 # less preprocessor
