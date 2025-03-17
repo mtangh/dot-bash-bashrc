7e5091a3ad50d55a7f481e85ea0f2f0c1a407dc0
MT <mtan.web+github@gmail.com>
2025-03-17 23:32:06 +0900

    Update

--- bash.bashrc.d/10_prompt.sh
+++ bash.bashrc.d/10_prompt.sh
@@ -5,53 +5,46 @@
 [[ "$-" = *i* ]] && [ -t 0 ] || return 0
 
 # Default PS*
+
 ## PS* Prefix
-PRE='\h'
 [ ${UID:--1} -ne 0 ] &&
-PRE='\u${SUDO_USER:+($SUDO_USER)}@'"${PRE}" ||
-PRE="${PRE}"
+PSU='\u${SUDO_USER:+($SUDO_USER)}@\h' ||
+PSU='\h'
 [ ${SHLVL:-1} -gt 1 ] && 
-PRE="${PRE}[${SHLVL}]" ||
-PRE="${PRE}"
-PRE="${PRE}"' \W'
-## Set PS*
-PS0=''
-PS1="${PRE}"'\$ '
-PS2="${PRE}"'> '
-PS3=' Select # => '
-PS4='+${BASH_SOURCE:+(${BASH_SOURCE##*/}${LINENO:+:$LINENO}):}'
-PS4="${PS4}"' ${FUNCNAME:+$FUNCNAME(): }'
-## Unset
-unset PRE
+PSU="${PSU}[${SHLVL}]" || :
 
 # PS* for TERM
-[ -t 0 ] &&
 case "${TERM:-}" in
 xterm*|screen*|tmux*|*-*color)
-  # Load the prompt color setting.
-  set +u
-  for ps_color in \
-  "${bashrc_userdir:-X}"/{,.}PS_COLOR{".$TERM",} \
-  "${bashrclocaldir:-X}"/PS_COLOR{".$TERM",}
-  do
-    [ -f "${ps_color}" ] &&
-    . "${ps_color}" &&
-    break || :
-  done &>/dev/null
-  unset ps_color
-  set -u
   # Set prompt color.
   [ ${UID:--1} -eq 0 ] && CLR='1;31' || CLR='1;34'
-  PS1="\[\e[${PS1COLOR:-$CLR}m\]${PS1}\[\e[0;39m\]"
-  PS2="\[\e[${PS2COLOR:-$CLR}m\]${PS2}\[\e[0;39m\]"
-  PS3="\[\e[${PS3COLOR:-1;37}m\]${PS3}\[\e[0;39m\]"
-  PS4="\[\e[${PS4COLOR:-1;30}m\]${PS4}\[\e[0;39m\]"
+  CL1="\[\e[${CLR}m\]"
+  CL2="\[\e[${CLR}m\]"
+  CL3="\[\e[1;37m\]"
+  CL4="\[\e[1;30m\]"
+  CLE="\[\e[0;39m\]"
   unset CLR
-  unset PS1COLOR PS2COLOR PS3COLOR PS4COLOR
   ;;
 *)
+  CL1=""
+  CL2=""
+  CL3=""
+  CL4=""
+  CLE=""
   ;;
 esac || :
+
+## Set PS*
+PS0=''
+PS1="${CL1)${PSU}${CLE} ${CL2}"'\W\$'"${CLE} "
+PS2="${CL1}${PSU}${CLE} ${CL2}"'\W>'"${CLE} "
+PS3=" ${CL3}Select # =>${CLE} "
+PS4='+${BASH_SOURCE:+(${BASH_SOURCE##*/}${LINENO:+:$LINENO}):}'
+PS4="${PS4}"'${FUNCNAME:+ $FUNCNAME():}'
+PS4="${CL4}${PS4}${CLE} "
+
+## Unset
+unset PRU CL1 CL2 CL3 CL4 CLE
 
 # Set bash.bashrc dir if undefined.
 [ ! -d "${bashbashrc_dir:-X}" ] && {
